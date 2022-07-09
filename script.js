f52079c7edc25f2e365d2f9c23c542756c00a963
pyaesonepsn <pyaesonepsn@gmail.com>
2022-07-10 00:51:45 +0630

    media query

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navbarBtn = document.querySelector('.navbar-btn');
-const navbarNav = document.querySelector('.navbar-btn');
+const navbarNav = document.querySelector('.navbar-nav');
 const dropdownContentFeatures = document.querySelector('.dropdown-content-features');
 const dropdownContentCompany = document.querySelector('.dropdown-content-company');
 const btnFeaturesIcon = document.getElementById('btn-features-icon');
@@ -9,17 +9,17 @@ const btnCompany = document.getElementById('btn-company');
 
 navbarBtn.addEventListener('click', () => {
     navbarBtn.classList.toggle('toggle');
-    navbarBtn.classList.toggle('show-nav');
+    navbarNav.classList.toggle('show-nav');
 });
 
 btnFeatures.addEventListener('click', () => {
     dropdownContentFeatures.classList.toggle('show-features')
     btnFeaturesIcon.classList.toggle('transform');
     btnFeatures.classList.toggle('toggle-color')
-})
+});
 
 btnCompany.addEventListener('click', () => {
     dropdownContentCompany.classList.toggle('show-company')
     btnCompanyIcon.classList.toggle('transform');
     btnCompany.classList.toggle('toggle-color');
-})
+});
